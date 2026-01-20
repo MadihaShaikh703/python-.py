f8a70654df1d0c1d319c9a51d793d3288584d399
MadihaShaikh703 <madihashaikh703@gmail.com>
2026-01-20 10:04:30 +0530

    Add files via upload

---
+++ exp2b.py
@@ -0,0 +1,5 @@
+# Calculation of area of a rectangle
+l=int(input("Enter the length of the rectangle : "))
+b=int(input("Enter the breadth of the rectangle : "))
+Area=l*b
+print(f"Area of rectangle is {Area} sq.cm")
