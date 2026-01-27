8f9d85af1d69d6ceb562148b7453dc07b9e71eca
MadihaShaikh703 <madihashaikh703@gmail.com>
2026-01-27 09:40:42 +0530

    Add files via upload

---
+++ exp3.py
@@ -0,0 +1,10 @@
+
+print("251A018 , 27/1/26")
+# calculation of gross salary
+BS=float(input("Enter the base salary value : "))
+DA=0.7*BS
+TA=0.3*BS
+HRA=0.1*BS
+GS=BS+DA+TA+HRA
+print(f"The Gross Salary is {GS}")
+      
