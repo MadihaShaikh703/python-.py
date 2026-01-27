7219a389c0c58d6deb4d75a328c5c491c3e5bc85
MadihaShaikh703 <madihashaikh703@gmail.com>
2026-01-27 09:44:28 +0530

    Add files via upload

---
+++ exp4.py
@@ -0,0 +1,7 @@
+print("251A018 , 27/1/26")
+#calculation of simple interest
+P=float(input("Enter the prinipal amount : "))
+R=float(input("Enter the rate of interest: "))
+T=float(input("Enter the time period : "))
+SI=P*R*T/100
+print(f"The Simple interest is {SI} ")
